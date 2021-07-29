a4b5a316b9f35c4d9b1201ba4ce7d1911296b072
Peter Millspaugh <peterdgmillspaugh@gmail.com>
2021-07-29 08:24:19 -0400

    Add favicon

--- pages/_document.js
+++ pages/_document.js
@@ -1,20 +1,21 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class GroovieDoc extends Document {
   render() {
     return (
       <Html>
         <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel='preconnect' href='https://fonts.googleapis.com' />
           <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            crossOrigin="true"
+            rel='preconnect'
+            href='https://fonts.gstatic.com'
+            crossOrigin='true'
           />
           <link
-            href="https://fonts.googleapis.com/css2?family=Shrikhand&display=swap"
-            rel="stylesheet"
+            href='https://fonts.googleapis.com/css2?family=Shrikhand&display=swap'
+            rel='stylesheet'
           />
+          <link rel='icon' href='./favicon.ico' />
         </Head>
         <body>
           <Main />
