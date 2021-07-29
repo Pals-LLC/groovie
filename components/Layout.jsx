4fcb41686edf86ffa612f14d4a719b5ff3509aa2
Peter Millspaugh <peterdgmillspaugh@gmail.com>
2021-07-29 08:37:11 -0400

    Extract page layout, nav link/button, and spacer into reusable components

---
+++ components/Layout.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const Layout = styled.div`
+  min-height: 100%;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+
+export default Layout;
